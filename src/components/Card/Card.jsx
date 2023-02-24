b0378f7364a764d210e40aeccf4b9f6cde9b6f82
prakash shrestha <rzprakash16@gmail.com>
2023-02-24 14:53:08 +0545

    map update

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import {  CardActionArea, } from '@mui/material';
+
+import OutdoorGrillOutlinedIcon from '@mui/icons-material/OutdoorGrillOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import LocalAtmOutlinedIcon from '@mui/icons-material/LocalAtmOutlined';
+import styled from 'styled-components';
+import CallOutlinedIcon from '@mui/icons-material/CallOutlined';
+const ContainerItem = styled.div`
+display:flex;
+padding:10px;
+width:100%;
+gap:5px;
+height:auto;`
+const Contain = styled.div`
+display:flex;
+align-items:center;
+`
+const InfoContainer = styled.div`
+display:flex;
+align-items:center;
+margin-top:5px;
+
+`
+console.log(Restaurants)
+export default function ActionAreaCard() {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="https://usmenuguide.com/wp-content/uploads/2022/06/yarsaNepaleseCuisinesanfrancisco404.jpg"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Yarsa Nepalese Cuisine 
+                    </Typography>
+                    <Typography variant='body3' color='gray'>North Beach
+                        San Francisco
+                        California
+
+                    </Typography>
+                    <InfoContainer>
+                        <CallOutlinedIcon fontSize='small'/>
+                        <Typography variant='body3' color='gray'>
+                            984-456790
+
+                        </Typography>
+                    </InfoContainer>
+
+
+                </CardContent>
+                <ContainerItem>
+                    <Contain>
+                        <LocationOnOutlinedIcon />
+                        <Typography variant='body3' color='gray'>1km</Typography>
+                    </Contain>
+                    <Contain>
+                        <OutdoorGrillOutlinedIcon />
+                        <Typography variant='body3' color='gray'>Nepali</Typography>
+                    </Contain>
+                    <Contain>
+                        <LocalAtmOutlinedIcon />
+                        <Typography variant='body3' color='gray'>100-200</Typography>
+                    </Contain>
+                </ContainerItem>
+            </CardActionArea>
+        </Card>
+    );
+}
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import {  MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+import New2 from '../../components/new2.png'
+const Map = () => {
+    
+    const[location, setLocation] = useState({lat:37.799193653119225, lng:-122.40726619122009})
+    
+    const data = [
+        {
+            lat:37.799193653119225, lng:-122.40726619122009,
+           
+            location:"nepal"
+        },
+        {
+            lat:38.799193653119225, lng:-123.40726619122009,
+            location:"nepal2"
+        },
+    ]
+   
+   const Icon = L.icon({
+    iconSize:[35, 40],
+    iconUrl: New2
+   })
+
+   useEffect(()=>{
+   
+      navigator.geolocation.getCurrentPosition(({coords:{latitude, longitude}})=>{
+setLocation({lat: latitude, lng:longitude})
+      })
+   },[])
+   
+
+   
+  return (
+    <MapContainer center={[location.lat, location.lng]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" style={{height:'100vh', width:'100vw'}}/>
+{
+    data?.map((item, index)=>(
+        <Marker position={[item.lat, item.lng]} icon={Icon} key={index}>
+        <Popup >
+            {item.location}
+        </Popup>
+            </Marker>
+    ))
+}
+    </MapContainer>
+  )
+}
+
+
+export default Map
